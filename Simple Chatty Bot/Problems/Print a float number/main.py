976a27b4edcbd6584f5f5447d7a9ad243c31db12
kimphuongg <phuong.tk.nguyen.111@gmail.com>
2021-04-20 13:10:20 +0700

    Update A Simple Chatty Bot

---
+++ Simple Chatty Bot/Problems/Print a float number/main.py
@@ -0,0 +1,2 @@
+float_number = 1.0000000000000001
+print(float_number)
---
+++ Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
@@ -0,0 +1,6 @@
+# write your code here
+bot_name = "Aid"
+birth_year = "2020"
+
+print(f"Hello! My name is {bot_name}.")
+print(f"I was created in {birth_year}")
